bd8f8c58a71ed42bae051ed33ff4767b8a463184
Scleepy <danielyohanes03@gmail.com>
2024-10-21 08:47:08 +0700

    Resolved env issue

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -1,23 +1,14 @@
 import { initializeApp, getApp, getApps } from '@react-native-firebase/app';
 import database from '@react-native-firebase/database';
-import {
-  FIREBASE_API_KEY,
-  FIREBASE_AUTH_DOMAIN,
-  FIREBASE_DATABASE_URL,
-  FIREBASE_PROJECT_ID,
-  FIREBASE_STORAGE_BUCKET,
-  FIREBASE_MESSAGING_SENDER_ID,
-  FIREBASE_APP_ID
-} from '@env';
 
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY || '',
-  authDomain: FIREBASE_AUTH_DOMAIN || '',
-  databaseURL: FIREBASE_DATABASE_URL || '',
-  projectId: FIREBASE_PROJECT_ID || '',
-  storageBucket: FIREBASE_STORAGE_BUCKET || '',
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID || '',
-  appId: FIREBASE_APP_ID || '',
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY || '',
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN || '',
+  databaseURL: process.env.FIREBASE_DATABASE_URL || '',
+  projectId: process.env.FIREBASE_PROJECT_ID || '',
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || '',
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || '',
+  appId: process.env.FIREBASE_APP_ID || '',
 };
 
 const firebaseApp = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
