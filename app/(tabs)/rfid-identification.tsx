0d579a06cb3850f7967c73d650a5deacc9f2eda7
Scleepy <danielyohanes03@gmail.com>
2024-11-03 17:53:55 +0700

    Added grams info

--- app/(tabs)/rfid-identification.tsx
+++ app/(tabs)/rfid-identification.tsx
@@ -160,7 +160,7 @@ const RFIDIdentification = () => {
                 ) : (
                   <>
                     <ThemedText style={styles.cardTitle}>
-                      {pet?.petName}
+                      {pet?.petName} ({pet?.dispenseAmount}gr)
                     </ThemedText>
                     <Text
                       style={styles.cardText}
